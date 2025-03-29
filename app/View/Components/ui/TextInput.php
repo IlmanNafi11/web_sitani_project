9e043fda5899d152705d2c9ab5967c81cdaa46b5
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-29 07:12:18 +0700

    membuat component text input

---
+++ app/View/Components/ui/TextInput.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\View\Components\ui;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class TextInput extends Component
+{
+    public $textLabel;
+    public $inputName;
+    public $inputPlaceholder;
+    public $inputType;
+    public $mode;
+    public $messageVisible;
+    public $errorMessage;
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct($textLabel, $inputName, $inputPlaceholder, $inputType, $mode, $errorMessage, $messageVisible)
+    {
+        $this->textLabel = $textLabel;
+        $this->inputName = $inputName;
+        $this->inputPlaceholder = $inputPlaceholder;
+        $this->inputType = $inputType;
+        $this->mode = $mode;
+        $this->errorMessage = $errorMessage;
+        $this->messageVisible = $messageVisible;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components..ui.text-input');
+    }
+}
