d5c2463848443eebeb72bac2ed18daa57a4ddd65
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-30 08:52:15 +0700

    membuat routing resource bibit, desa, kecamatan, kelompok tani, komoditas, dan penyuluh terdaftar

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Http\Controllers\BibitBerkualitasController;
+use App\Http\Controllers\DesaController;
+use App\Http\Controllers\KecamatanController;
+use App\Http\Controllers\KelompokTaniController;
+use App\Http\Controllers\KomoditasController;
+use App\Http\Controllers\PenyuluhTerdaftarController;
 use Illuminate\Support\Facades\Route;
 
+// sementara
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.login');
 });
+Route::get('/dashboard', function() {
+    return view('pages.dashboard');
+})->name('dashboard.admin');
+
+Route::resource('bibit', BibitBerkualitasController::class);
+Route::resource('desa', DesaController::class);
+Route::resource('kecamatan', KecamatanController::class);
+Route::resource('kelompok-tani', KelompokTaniController::class);
+Route::resource('komoditas', KomoditasController::class);
+Route::resource('penyuluh-terdaftar', PenyuluhTerdaftarController::class);
