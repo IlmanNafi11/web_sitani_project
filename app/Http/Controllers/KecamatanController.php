4a3349bc5295e490c9422e12fdca6039c050a6a7
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-04 17:11:54 +0700

    implementasi fungsional view dan create resource

--- app/Http/Controllers/KecamatanController.php
+++ app/Http/Controllers/KecamatanController.php
@@ -2,16 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\KecamatanRequest;
+use App\Services\KecamatanService;
 use Illuminate\Http\Request;
 
 class KecamatanController extends Controller
 {
+    protected KecamatanService $kecamatanService;
+
+    public function __construct(KecamatanService $kecamatanService)
+    {
+        $this->kecamatanService = $kecamatanService;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $kecamatans = $this->kecamatanService->getAll();
+        return view('pages.kecamatan.index', compact('kecamatans'));
     }
 
     /**
@@ -19,15 +28,20 @@ class KecamatanController extends Controller
      */
     public function create()
     {
-        //
+        return view('pages.kecamatan.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(KecamatanRequest $request)
     {
-        //
+        $validated = $this->kecamatanService->create($request->validated());
+        if ($validated) {
+            return redirect()->route('kecamatan.index')->with('success', 'Data Berhasil disimpan');
+        }
+        
+        return redirect()->route('kecamatan.create')->with('failed', 'Data Gagal disimpan');
     }
 
     /**
