02ddde327cfbdcba5f306b65318f6a01a8f9ac70
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-04 22:27:46 +0700

    membuat desa repository

---
+++ app/Repositories/DesaRepository.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Repositories;
+
+use App\Repositories\Interfaces\DesaRepositoryInterface;
+
+class DesaRepository implements DesaRepositoryInterface {
+
+    public function getAll()
+    {
+
+    }
+
+    public function find($id)
+    {
+
+    }
+
+    public function create(array $data)
+    {
+
+    }
+
+    public function update($id, array $data)
+    {
+
+    }
+
+    public function delete($id)
+    {
+        
+    }
+}
