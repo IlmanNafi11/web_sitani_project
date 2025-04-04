fa482f916a394f687d4f43af200ffe4170d0f697
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-04 22:31:52 +0700

    implementasi kontrak

--- app/Repositories/DesaRepository.php
+++ app/Repositories/DesaRepository.php
@@ -1,32 +1,33 @@
 <?php
 namespace App\Repositories;
 
+use App\Models\Desa;
 use App\Repositories\Interfaces\DesaRepositoryInterface;
 
 class DesaRepository implements DesaRepositoryInterface {
 
     public function getAll()
     {
-
+        return Desa::all();
     }
 
     public function find($id)
     {
-
+        return Desa::find($id);
     }
 
     public function create(array $data)
     {
-
+        return Desa::create($data);
     }
 
     public function update($id, array $data)
     {
-
+        return Desa::where('id', $id)->update($data);
     }
 
     public function delete($id)
     {
-        
+        return Desa::destroy($id);
     }
 }
