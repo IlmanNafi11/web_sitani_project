c9077a8bd412e0afd6c1b69dc010a6c9faf0488d
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-05 02:07:55 +0700

    implementasi fungsional crud resource desa

--- app/Http/Controllers/DesaController.php
+++ app/Http/Controllers/DesaController.php
@@ -2,32 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\DesaRequest;
+use App\Services\DesaService;
+use App\Services\KecamatanService;
 use Illuminate\Http\Request;
 
 class DesaController extends Controller
 {
+    protected DesaService $desaService;
+
+    public function __construct(DesaService $desaService)
+    {
+        $this->desaService = $desaService;
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        return view('pages.desa.index');
+        $desas = $this->desaService->getAllWithKecamatan();
+        return view('pages.desa.index', compact('desas'));
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(KecamatanService $kecamatanService)
     {
-        //
+        $kecamatans = $kecamatanService->getAll();
+        return view('pages.desa.create', compact('kecamatans'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(DesaRequest $request)
     {
-        //
+        $result = $this->desaService->create($request->validated());
+
+        if ($result) {
+            return redirect()->route('desa.index')->with('success', 'Data berhasil disimpan');
+        }
+
+        return redirect()->route('desa.index')->with('failed', 'Data gagal disimpan');
     }
 
     /**
@@ -41,17 +59,25 @@ class DesaController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id, KecamatanService $kecamatanService)
     {
-        //
+        $kecamatans = $kecamatanService->getAll();
+        $desa = $this->desaService->getById($id);
+        return view('pages.desa.update', compact('kecamatans', 'desa'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(DesaRequest $request, string $id)
     {
-        //
+        $result = $this->desaService->update($id, $request->validated());
+
+        if ($result) {
+            return redirect()->route('desa.index')->with('success', 'Data berhasil diperbarui');
+        }
+
+        return redirect()->route('desa.index')->with('failed', 'Gagal memperbarui data desa');
     }
 
     /**
@@ -59,6 +85,8 @@ class DesaController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $this->desaService->delete($id);
+
+        return redirect()->route('desa.index')->with('success', 'Data berhasil dihapus');
     }
 }
--- app/Services/DesaService.php
+++ app/Services/DesaService.php
@@ -1,13 +1,73 @@
 <?php
 
-use App\Repositories\Interfaces\DesaRepositoryInterface;
+namespace App\Services;
 
-class DesaService {
+use App\Repositories\Interfaces\DesaRepositoryInterface;
+use Illuminate\Support\Facades\Log;
+
+class DesaService
+{
 
     protected DesaRepositoryInterface $desaRepository;
 
     public function __construct(DesaRepositoryInterface $desaRepository)
     {
         $this->desaRepository = $desaRepository;
+    }
+
+    public function getAll()
+    {
+        try {
+            return $this->desaRepository->getAll();
+        } catch (\Throwable $th) {
+            Log::error('Gagal mengambil seluruh data desa: ' . $th->getMessage());
+        }
+    }
+
+    public function getAllWithKecamatan()
+    {
+        try {
+            $desas = $this->desaRepository->getAll();
+
+            return $desas->load('kecamatan');
+        } catch (\Throwable $th) {
+            Log::error('Gagal mengambil seluruh data desa dengan kecamatan: ' . $th->getMessage());
+        }
+    }
+
+    public function create(array $data)
+    {
+        try {
+            return $this->desaRepository->create($data);
+        } catch (\Throwable $th) {
+            Log::error('Gagal menyimpan data desa: ' . $th->getMessage());
+        }
+    }
+
+    public function getById($id)
+    {
+        try {
+            return $this->desaRepository->find($id);
+        } catch (\Throwable $th) {
+            Log::error('Gagal mengambil data desa berdasarkan id: ' . $th->getMessage());
+        }
+    }
+
+    public function update($id, array $data)
+    {
+        try {
+            return $this->desaRepository->update($id, $data);
+        } catch (\Throwable $th) {
+            Log::error('Gagal memperbarui data desa: ' . $th->getMessage());
+        }
+    }
+
+    public function delete($id)
+    {
+        try {
+            return $this->desaRepository->delete($id);
+        } catch (\Throwable $th) {
+            Log::error('Gagal menghapus data desa: ' . $th->getMessage());
+        }
     }
 }
