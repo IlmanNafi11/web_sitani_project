3fec57095f874348d75e98989ef9bd77793f5462
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-04 17:09:31 +0700

    membuat kecamatan repository

---
+++ app/Repositories/KecamatanRepository.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Repositories;
+
+use App\Models\Kecamatan;
+use App\Repositories\Interfaces\KecamatanRepositoryInterfaces;
+
+class KecamatanRepository implements KecamatanRepositoryInterfaces{
+
+    public function getAll()
+    {
+        return Kecamatan::all('id', 'nama');
+    }
+
+    public function create(array $data)
+    {
+        return Kecamatan::create($data);
+    }
+
+    public function update($id, array $data)
+    {
+
+    }
+
+    public function find($id)
+    {
+
+    }
+
+    public function delete($id)
+    {
+
+    }
+}
