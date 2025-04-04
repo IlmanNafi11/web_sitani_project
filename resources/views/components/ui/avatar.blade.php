e2db9c593c9a211c67b6ffbcaf2c89e0cc31802d
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-04 16:05:29 +0700

    remove url image(sementara dlm proses development)

--- resources/views/components/ui/avatar.blade.php
+++ resources/views/components/ui/avatar.blade.php
@@ -1,3 +1,3 @@
 <div class="w-fit h-fit">
-    <img class="w-10 h-10 rounded-full shadow-sm" src="https://avatar.iran.liara.run/public" alt="Rounded avatar">
+    <img class="w-10 h-10 rounded-full shadow-sm" src="#" alt="Rounded avatar">
 </div>
