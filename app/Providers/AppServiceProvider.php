54e87ffd0c3b4f6f6effe44897de5902b2086227
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-06 02:06:20 +0700

    binding penyuluh interface dengan penyuluh terdaftar repository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,8 +8,10 @@ use App\Repositories\Interfaces\BibitRepositoryInterface;
 use App\Repositories\Interfaces\DesaRepositoryInterface;
 use App\Repositories\Interfaces\KecamatanRepositoryInterfaces;
 use App\Repositories\Interfaces\KomoditasRepositoryInterface;
+use App\Repositories\Interfaces\PenyuluhRepositoryInterface;
 use App\Repositories\KecamatanRepository;
 use App\Repositories\KomoditasRepository;
+use App\Repositories\PenyuluhTerdaftarRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(DesaRepositoryInterface::class, DesaRepository::class);
         $this->app->bind(KomoditasRepositoryInterface::class, KomoditasRepository::class);
         $this->app->bind(BibitRepositoryInterface::class, BibitRepository::class);
+        $this->app->bind(PenyuluhRepositoryInterface::class, PenyuluhTerdaftarRepository::class);
     }
 
     /**
