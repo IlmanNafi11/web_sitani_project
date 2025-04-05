cddfbe9b989f654f458d53b8212043678d13b523
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-05 07:36:25 +0700

    menambahkan routing list view data komoditas

--- resources/views/components/partials/side-bar.blade.php
+++ resources/views/components/partials/side-bar.blade.php
@@ -41,6 +41,10 @@
                             class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Bibit</a>
                     </li>
                     <li>
+                        <a href="{{ route('komoditas.index') }}"
+                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Komoditas</a>
+                    </li>
+                    <li>
                         <a href="#"
                             class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Penyuluh</a>
                     </li>
