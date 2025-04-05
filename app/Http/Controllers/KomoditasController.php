0dafa74b92adabc40f7dd320b506bc366f4f561d
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-05 09:20:53 +0700

    implementasi fungsional crud komoditas resource

--- app/Http/Controllers/KomoditasController.php
+++ app/Http/Controllers/KomoditasController.php
@@ -2,16 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\KomoditasRequest;
+use App\Services\KomoditasService;
 use Illuminate\Http\Request;
 
 class KomoditasController extends Controller
 {
+
+    protected KomoditasService $komoditasService;
+
+    public function __construct(KomoditasService $komoditasService)
+    {
+        $this->komoditasService = $komoditasService;
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $datas = $this->komoditasService->getAll();
+
+        return view('pages.komoditas.index', compact('datas'));
     }
 
     /**
@@ -19,15 +31,21 @@ class KomoditasController extends Controller
      */
     public function create()
     {
-        //
+        return view('pages.komoditas.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(KomoditasRequest $request)
     {
-        //
+        $result = $this->komoditasService->create($request->validated());
+
+        if ($result) {
+            return redirect()->route('komoditas.index')->with('success', 'Data berhasil disimpan');
+        }
+
+        return redirect()->route('komoditas.index')->with('failed', 'Data gagal disimpan');
     }
 
     /**
@@ -43,15 +61,23 @@ class KomoditasController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $komoditas = $this->komoditasService->getById($id);
+
+        return view('pages.komoditas.update', compact('komoditas'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(KomoditasRequest $request, string $id)
     {
-        //
+        $result = $this->komoditasService->update($id, $request->validated());
+
+        if ($result) {
+            return redirect()->route('komoditas.index')->with('success', 'Data berhasil diperbarui');
+        }
+
+        return redirect()->route('komoditas.index')->with('failed', 'Data gagal diperbarui');
     }
 
     /**
@@ -59,6 +85,8 @@ class KomoditasController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $this->komoditasService->delete($id);
+
+        return redirect()->route('komoditas.index')->with('success', 'Data berhasil dihapus');
     }
 }
