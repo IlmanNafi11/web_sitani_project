f3b406de1885af6167e29d5d15c4e47b8e532a66
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-08 04:34:08 +0700

    binding interface custom query ke repository terkait

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,13 +5,21 @@ namespace App\Providers;
 use App\Repositories\BibitRepository;
 use App\Repositories\DesaRepository;
 use App\Repositories\Interfaces\BibitRepositoryInterface;
+use App\Repositories\Interfaces\DesaCustomQueryInterface;
 use App\Repositories\Interfaces\DesaRepositoryInterface;
 use App\Repositories\Interfaces\KecamatanRepositoryInterfaces;
+use App\Repositories\Interfaces\KelompokTaniRepositoryInterface;
 use App\Repositories\Interfaces\KomoditasRepositoryInterface;
+use App\Repositories\Interfaces\ManyRelationshipManagement;
 use App\Repositories\Interfaces\PenyuluhRepositoryInterface;
+use App\Repositories\Interfaces\PenyuluhTerdaftarCustomQueryInterface;
 use App\Repositories\KecamatanRepository;
+use App\Repositories\KelompokTaniRepository;
 use App\Repositories\KomoditasRepository;
 use App\Repositories\PenyuluhTerdaftarRepository;
+use App\Services\DesaService;
+use App\Services\KelompokTaniService;
+use App\Services\PenyuluhTerdaftarService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,6 +34,10 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(KomoditasRepositoryInterface::class, KomoditasRepository::class);
         $this->app->bind(BibitRepositoryInterface::class, BibitRepository::class);
         $this->app->bind(PenyuluhRepositoryInterface::class, PenyuluhTerdaftarRepository::class);
+        $this->app->bind(KelompokTaniRepositoryInterface::class, KelompokTaniRepository::class);
+        $this->app->when(KelompokTaniService::class)->needs(ManyRelationshipManagement::class)->give(KelompokTaniRepository::class);
+        $this->app->when(DesaService::class)->needs(DesaCustomQueryInterface::class)->give(DesaRepository::class);
+        $this->app->when(PenyuluhTerdaftarService::class)->needs(PenyuluhTerdaftarCustomQueryInterface::class)->give(PenyuluhTerdaftarRepository::class);
     }
 
     /**
