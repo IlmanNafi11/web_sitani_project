2e9184642b9360a2f3cb2f568ea92b47a392668d
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-07 13:55:51 +0700

    menambahkan routing list view kelompok tani

--- resources/views/components/partials/side-bar.blade.php
+++ resources/views/components/partials/side-bar.blade.php
@@ -49,7 +49,7 @@
                             class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Penyuluh</a>
                     </li>
                     <li>
-                        <a href="#"
+                        <a href="{{ route('kelompok-tani.index') }}"
                             class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Kelompok Tani</a>
                     </li>
                 </ul>
