32c70cd2016e69fecd114c36ae07249043f312b5
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-08 07:23:15 +0700

    menambahkan parameter withrelationship untuk memungkinkan get data lengkap dengan relasinya

--- app/Repositories/Interfaces/KelompokTaniRepositoryInterface.php
+++ app/Repositories/Interfaces/KelompokTaniRepositoryInterface.php
@@ -4,10 +4,9 @@ namespace App\Repositories\Interfaces;
 
 interface KelompokTaniRepositoryInterface
 {
-    public function getAll();
+    public function getAll($withRelations = false);
     public function create(array $data);
     public function update($id, array $data);
     public function find($id);
     public function delete($id);
-    public function syncPenyuluh($kelompokTaniId, array $penyuluhIds): void;
 }
