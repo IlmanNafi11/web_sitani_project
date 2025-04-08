58bafe8171f9635d189403ddc760e7f3d8733024
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-08 07:28:57 +0700

    implementasi fungsional crud

--- app/Http/Controllers/KelompokTaniController.php
+++ app/Http/Controllers/KelompokTaniController.php
@@ -2,32 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\KelompokTaniRequest;
+use App\Services\DesaService;
+use App\Services\KecamatanService;
+use App\Services\KelompokTaniService;
 use Illuminate\Http\Request;
 
 class KelompokTaniController extends Controller
 {
+    protected KelompokTaniService $kelompokTaniService;
+
+    public function __construct(KelompokTaniService $kelompokTaniService)
+    {
+        $this->kelompokTaniService = $kelompokTaniService;
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $kelompokTanis = $this->kelompokTaniService->getAllWithRelations();
+        return view('pages.kelompok_tani.index', compact('kelompokTanis'));
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(KecamatanService $kecamatanService)
     {
-        //
+        $kecamatans = $kecamatanService->getAll();
+        return view('pages.kelompok_tani.create', compact('kecamatans'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(KelompokTaniRequest $request)
     {
-        //
+        $result = $this->kelompokTaniService->create($request->validated());
+
+        if ($result) {
+            return redirect()->route('kelompok-tani.index')->with('success', 'Data berhasil disimpan');
+        }
+
+        return redirect()->route('kelompok-tani.index')->with('success', 'Data gagal disimpan');
     }
 
     /**
@@ -41,17 +60,24 @@ class KelompokTaniController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id, KecamatanService $kecamatanService)
     {
-        //
+        $kelompokTanis = $this->kelompokTaniService->getByIdWithPivot($id);
+        $kecamatans = $kecamatanService->getAll();
+        return view('pages.kelompok_tani.update', compact('kelompokTanis', 'kecamatans'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(KelompokTaniRequest $request, string $id)
     {
-        //
+        $result = $this->kelompokTaniService->update($id, $request->validated());
+        if ($result) {
+            return redirect()->route('kelompok-tani.index')->with('success', 'Data berhasil diperbarui');
+        }
+
+        return redirect()->route('kelompok-tani.index')->with('failed', 'Data gagal diperbarui');
     }
 
     /**
@@ -59,6 +85,8 @@ class KelompokTaniController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $this->kelompokTaniService->delete($id);
+
+        return redirect()->route('kelompok-tani.index')->with('success', 'Data berhasil dihapus');
     }
 }
