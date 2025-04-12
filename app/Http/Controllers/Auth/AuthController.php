822b259b86504c1a70a506e7943eb5705a7ff3af
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-12 14:09:14 +0700

    membuat authentication controller

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Facades\JWTFactory;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (! $token = JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => JWTFactory::getTTL(),
+        ]);
+    }
+}
