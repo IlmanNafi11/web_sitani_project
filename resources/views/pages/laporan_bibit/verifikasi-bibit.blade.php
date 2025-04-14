994fb4e5c425f36334be3035153cc1cfba34640e
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-15 05:12:21 +0700

    membuat halaman verifikasi laporan bibit

---
+++ resources/views/pages/laporan_bibit/verifikasi-bibit.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.layout')
+@section('content')
+    <div class="kelompok-tani-page-content flex flex-col w-full h-screen relative overflow-hidden">
+        <x-partials.header />
+        <div class="content-container flex w-full h-[calc(100vh-70px)] absolute top-[70px]">
+            <x-partials.side-bar />
+            <div class="content flex-1 w-full p-8 flex">
+                <div
+                    class="main-content w-full overflow-y-auto p-6 rounded-xl bg-white border border-[#E5E8EC] h-fit max-h-full">
+                    <div class="flex flex-col gap-4">
+                        <div class="border-b border-[#E5E8EC]">
+                            <span class="title-label-page font-medium mb-4 block">Verifikasi laporan</span>
+                        </div>
+                        <div class="form-container">
+                            <x-form.form-bibit-verify />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+        crossorigin="anonymous"></script>
+@endsection
