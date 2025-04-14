c3f91f0f89845445a17378ab1c6c1b25d5a50cbc
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-15 05:11:19 +0700

    memperbarui style button save dengan style btn-soft accent color

--- resources/views/components/ui/button-save.blade.php
+++ resources/views/components/ui/button-save.blade.php
@@ -1,12 +1,6 @@
-<button class="simpan-button cursor-pointer bg-blue-600 hover:bg-blue-800 font-medium rounded-lg p-2.5 text-white flex gap-1"
+<button class="simpan-button btn btn-soft btn-accent"
     type="button" id="btn-simpan">
-    <svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-        fill="none" viewBox="0 0 24 24">
-        <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
-            d="M11 16h2m6.707-9.293-2.414-2.414A1 1 0 0 0 16.586 4H5a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V7.414a1 1 0 0 0-.293-.707ZM16 20v-6a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v6h8ZM9 4h6v3a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1V4Z" />
-    </svg>
-    <span>{{ $title ?? 'Simpan'}}</span>
-</button>
+{{ $title ?? 'Simpan'}}</button>
 
 @once
     <script>
