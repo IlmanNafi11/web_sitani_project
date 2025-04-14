ab2fdff8613bc580b489e844f67e296194d8dd21
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-14 16:19:16 +0700

    mengatur routing untuk api

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,26 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\KomoditasController;
+use App\Http\Controllers\KelompokTaniController;
+use App\Http\Controllers\BibitBerkualitasController;
+use App\Http\Controllers\LaporanBibitController;
+use App\Http\Middleware\JwtMiddleware;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->withoutMiddleware(JwtMiddleware::class);
+
+Route::middleware('api')->group(function() {
+    // Route::get('/user/{id}', [UserController::class, 'getUserById'])->whereNumber('id');
+    Route::get('/komoditas', [KomoditasController::class, 'getAll']);
+    Route::get('/komoditas/{id}', [KomoditasController::class, 'getById']);
+    Route::get('/kelompok-tani', [KelompokTaniController::class, 'getAllByPenyuluh']);
+    Route::get('/kelompok-tani/{id}', [KelompokTaniController::class, 'getById']);
+    Route::get('/bibit', [BibitBerkualitasController::class, 'getAll']);
+    Route::post('/laporan-kondisi', [LaporanBibitController::class, 'store']);
+});
