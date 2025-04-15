ed489d9acd8ae3783f0b5b625a6d5462ed362353
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-15 15:52:22 +0700

    implementasi ui dengan komponen yang baru

--- resources/views/pages/laporan_bibit/index.blade.php
+++ resources/views/pages/laporan_bibit/index.blade.php
@@ -30,28 +30,32 @@
                                 <tr class="text-black">
                                     <td>{{ $laporan->kelompokTani->nama }}</td>
                                     <td>{{ $laporan->komoditas->nama }}</td>
-                                    <td>{{ $laporan->laporanKondisiDetail->estimasi_panen }}</td>
+                                    <td>
+                                        <x-ui.badge :color="'badge-info'" :style="'badge-soft'" :icon="'icon-[heroicons--calendar-date-range]'" :title="$laporan->laporanKondisiDetail->estimasi_panen"/>
+                                    </td>
                                     <td>{{ $laporan->penyuluh->penyuluhTerdaftar->nama }}</td>
-                                    <td>{{ $laporan->created_at }}</td>
+                                    <td>
+                                        <x-ui.badge :color="'badge-accent'" :style="'badge-soft'" :icon="'icon-[fluent--document-bullet-list-clock-20-regular]'" :title="$laporan->created_at" />
+                                    </td>
                                     @php
                                         $statusMap = [
-                                            "1" => ['title' => 'Berkualitas', 'color' => 'badge-success'],
-                                            "0" => ['title' => 'Tidak Berkualitas', 'color' => 'badge-error'],
-                                            "2" => ['title' => 'Menunggu Verifikasi', 'color' => 'badge-warning'],
+                                            "1" => ['title' => 'Berkualitas', 'color' => 'badge-success', 'icon' => 'icon-[solar--verified-check-bold]'],
+                                            "0" => ['title' => 'Tidak Berkualitas', 'color' => 'badge-error', 'icon' => 'icon-[lucide--badge-alert]'],
+                                            "2" => ['title' => 'Menunggu Verifikasi', 'color' => 'badge-warning', 'icon' => 'icon-[ph--clock-user-light]'],
                                         ];
 
                                         $statusData = $statusMap[$laporan->status] ?? ['title' => 'Status tidak diketahui', 'color' => 'badge-secondary'];
                                     @endphp
                                     <td>
-                                        <x-ui.badge :color="$statusData['color']" :title="$statusData['title']" :style="'badge-soft'">
+                                        <x-ui.badge :color="$statusData['color']" :title="$statusData['title']" :style="'badge-soft'" :icon="$statusData['icon']">
                                             {{ $statusData['title'] }}
                                         </x-ui.badge>
                                     </td>
                                     <td>
-                                    <x-ui.dropdown-action :title="'Lihat Aksi'">
-                                        <x-ui.button-edit id="{{$laporan->id}}" route="laporan-bibit.edit" title="Verifikasi"/>
-                                        <x-ui.button-delete id="{{$laporan->id}}" route="laporan-bibit.destroy"/>
-                                    </x-ui.dropdown-action>
+                                        <x-ui.dropdown-action :title="'Lihat Aksi'">
+                                            <x-ui.button.edit-button :route="route('laporan-bibit.edit', $laporan->id)" :title="'Verifikasi'" :style="'btn-soft'" :color="'btn-success'" :icon="'icon-[line-md--check-list-3-filled]'"/>
+                                        <x-ui.button.delete-button :route="route('laporan-bibit.destroy', $laporan->id)" :keyId="$laporan->id" :color="'btn-error'" :style="'btn-soft'" :title="'Hapus'" :icon="'icon-[line-md--document-delete]'"/>
+                                        </x-ui.dropdown-action>
                                     </td>
                                 </tr>
                             @empty
