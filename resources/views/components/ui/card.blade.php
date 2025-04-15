088a027b8c19f1ae11ed2f08e99460d67e440574
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-15 21:59:40 +0700

    menambahkan padding

--- resources/views/components/ui/card.blade.php
+++ resources/views/components/ui/card.blade.php
@@ -1,3 +1,3 @@
-<div class="w-auto h-auto overflow-x-hidden overflow-y-auto border border-[#E5E8EC] rounded-xl bg-white">
+<div class="w-auto h-auto overflow-x-hidden overflow-y-auto border border-[#E5E8EC] rounded-xl bg-white p-6">
     {{ $slot }}
 </div>
