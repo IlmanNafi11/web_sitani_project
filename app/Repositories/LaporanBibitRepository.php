772d99fae84c0e674f88b9b0da926963ee2211fb
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-15 09:31:07 +0700

    implementasi metode getAll dengan relasi dan find berdasarkan id

--- app/Repositories/LaporanBibitRepository.php
+++ app/Repositories/LaporanBibitRepository.php
@@ -10,12 +10,52 @@ class LaporanBibitRepository implements CrudInterface{
 
     public function getAll($withRelations = false): Collection|array
     {
+        if ($withRelations) {
+            return LaporanKondisi::select(['id', 'status', 'created_at', 'kelompok_tani_id', 'komoditas_id', 'penyuluh_id'])->with([
+                'kelompokTani' => function($query){
+                    $query->select(['id', 'nama']);
+                },
+                'komoditas' => function ($query) {
+                    $query->select(['id', 'nama', 'musim']);
+                },
+                'penyuluh' => function ($query) {
+                    $query->select(['id', 'penyuluh_terdaftar_id']);
+                },
+                'penyuluh.penyuluhTerdaftar' => function ($query) {
+                    $query->select(['id', 'nama', 'no_hp']);
+                },
+            ])->with('laporanKondisiDetail')->get();
+        }
+
         return LaporanKondisi::all();
     }
 
     public function find($id): Model|Collection|null
     {
-        return LaporanKondisi::find($id);
+        return LaporanKondisi::with([
+            'kelompokTani' => function ($query) {
+                $query->select(['id', 'nama', 'desa_id', 'kecamatan_id']);
+            },
+            'kelompokTani.desa' => function ($query) {
+                $query->select(['id', 'nama']);
+            },
+            'kelompokTani.kecamatan' => function ($query) {
+                $query->select(['id', 'nama']);
+            },
+            'komoditas' => function ($query) {
+                $query->select(['id', 'nama', 'musim']);
+            },
+            'penyuluh' => function ($query) {
+                $query->select(['id', 'penyuluh_terdaftar_id']);
+            },
+            'penyuluh.penyuluhTerdaftar' => function ($query) {
+                $query->select(['id', 'nama', 'no_hp']);
+            },
+            'laporanKondisiDetail'
+        ])
+            ->select(['id', 'status', 'created_at', 'kelompok_tani_id', 'komoditas_id', 'penyuluh_id'])
+            ->where('id', $id)
+            ->first();
     }
 
     public function create(array $data): ?Model
