4743c90933886c8f7b74419da434069fa2000f88
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-15 21:01:19 +0700

    mempebarui implementasi komponen side-bar dengan style yang baru

--- resources/views/components/partials/side-bar.blade.php
+++ resources/views/components/partials/side-bar.blade.php
@@ -1,127 +1,102 @@
-<aside id="sidebar-multi-level-sidebar"
-    class="z-40 w-80 h-full transition-transform -translate-x-full sm:translate-x-0 sm:relative sm:block sm:top-0 top-[70px] fixed bg-white dark:bg-gray-800"
-    aria-label="Sidebar">
-    <div class="h-full px-3 py-4 overflow-y-auto bg-white dark:bg-gray-800">
-        <ul class="space-y-2 font-medium">
-            {{-- Menu Beranda --}}
+<aside id="with-navbar-sidebar"
+    class="overlay [--auto-close:sm] sm:shadow-none overlay-open:translate-x-0 drawer drawer-start hidden max-w-64 sm:absolute sm:z-0 sm:flex sm:translate-x-0 pt-16"
+    role="dialog" tabindex="-1">
+    <div class="drawer-body px-2 pt-4">
+        <ul class="menu p-0">
             <li>
-                <a href="#"
-                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                        <path
-                            d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                        <path
-                            d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-                    </svg>
-                    <span class="ms-3">Beranda</span>
+                <a href="#">
+                    <span class="icon-[tabler--home] size-5"></span>
+                    Beranda
                 </a>
             </li>
-
-            {{-- Menu Manajemen Data Pertanian --}}
-            <li>
-                <button type="button"
-                    class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                    aria-controls="dropdown-pertanian" data-collapse-toggle="dropdown-pertanian">
-                    <svg class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
-                        <path
-                            d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z" />
-                    </svg>
-                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Kelola Data Pertanian</span>
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 10 6">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="m1 1 4 4 4-4" />
-                    </svg>
-                </button>
-                <ul id="dropdown-pertanian" class="hidden py-2 space-y-2">
+            <li class="space-y-0.5">
+                <a class="collapse-toggle collapse-open:bg-base-content/10" id="menu-data-pertanian" data-collapse="#menu-data-pertanian-collapse">
+                    <span class="icon-[ph--farm] size-5"></span>
+                    Data Pertanian
+                    <span class="icon-[tabler--chevron-down] collapse-open:rotate-180 size-4 transition-all duration-300"></span>
+                </a>
+                <ul id="menu-data-pertanian-collapse"
+                    class="collapse hidden w-auto space-y-0.5 overflow-hidden transition-[height] duration-300"
+                    aria-labelledby="menu-data-pertanian">
                     <li>
-                        <a href="{{ route('bibit.index') }}"
-                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Bibit</a>
+                        <a href="{{ route('bibit.index') }}">
+                            <span class="icon-[flowbite--seedling-outline] size-5"></span>
+                            Bibit
+                        </a>
                     </li>
                     <li>
-                        <a href="{{ route('komoditas.index') }}"
-                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Komoditas</a>
+                        <a href="{{ route('komoditas.index') }}">
+                            <span class="icon-[icon-park-outline--commodity] size-5"></span>
+                            Komoditas
+                        </a>
                     </li>
                     <li>
-                        <a href="{{ route('penyuluh-terdaftar.index') }}"
-                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Penyuluh</a>
+                        <a href="{{ route('penyuluh-terdaftar.index') }}">
+                            <span class="icon-[ph--user] size-5"></span>
+                            Penyuluh
+                        </a>
                     </li>
                     <li>
-                        <a href="{{ route('kelompok-tani.index') }}"
-                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Kelompok Tani</a>
+                        <a href="{{ route('kelompok-tani.index') }}">
+                            <span class="icon-[fluent--group-24-regular] size-5"></span>
+                            Kelompok Tani
+                        </a>
                     </li>
                 </ul>
             </li>
 
-            {{--  Menu Manajemen Data Wilayah --}}
-            <li>
-                <button type="button"
-                    class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                    aria-controls="dropdown-wilayah" data-collapse-toggle="dropdown-wilayah">
-                    <svg class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
-                        <path
-                            d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z" />
-                    </svg>
-                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Kelola Data Wilayah</span>
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="m1 1 4 4 4-4" />
-                    </svg>
-                </button>
-                <ul id="dropdown-wilayah" class="hidden py-2 space-y-2">
+            <li class="space-y-0.5">
+                <a class="collapse-toggle collapse-open:bg-base-content/10" id="menu-data-wilayah" data-collapse="#menu-data-wilayah-collapse">
+                    <span class="icon-[oui--vis-map-region] size-5"></span>
+                    Data Wilayah
+                    <span class="icon-[tabler--chevron-down] collapse-open:rotate-180 size-4 transition-all duration-300"></span>
+                </a>
+                <ul id="menu-data-wilayah-collapse"
+                    class="collapse hidden w-auto space-y-0.5 overflow-hidden transition-[height] duration-300"
+                    aria-labelledby="menu-data-wilayah">
                     <li>
-                        <a href="{{ route('desa.index') }}"
-                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Desa</a>
+                        <a href="{{ route('desa.index') }}">
+                            <span class="icon-[healthicons--village-outline-24px] size-5"></span>
+                            Desa
+                        </a>
                     </li>
                     <li>
-                        <a href="{{ route('kecamatan.index') }}"
-                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Kecamatan</a>
+                        <a href="{{ route('kecamatan.index') }}">
+                            <span class="icon-[material-symbols--holiday-village] size-5"></span>
+                            Kecamatan
+                        </a>
                     </li>
                 </ul>
             </li>
 
-            {{--  Menu Manajemen Laporan --}}
-            <li>
-                <button type="button"
-                    class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                    aria-controls="dropdown-laporan" data-collapse-toggle="dropdown-laporan">
-                    <svg class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
-                        <path
-                            d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z" />
-                    </svg>
-                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Kelola Data Laporan</span>
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="m1 1 4 4 4-4" />
-                    </svg>
-                </button>
-                <ul id="dropdown-laporan" class="hidden py-2 space-y-2">
+            <li class="space-y-0.5">
+                <a class="collapse-toggle collapse-open:bg-base-content/10" id="menu-data-laporan"
+                    data-collapse="#menu-data-laporan-collapse">
+                    <span class="icon-[icon-park-outline--table-report] size-5"></span>
+                    Data Laporan
+                    <span class="icon-[tabler--chevron-down] collapse-open:rotate-180 size-4 transition-all duration-300"></span>
+                </a>
+                <ul id="menu-data-laporan-collapse"
+                    class="collapse hidden w-auto space-y-0.5 overflow-hidden transition-[height] duration-300"
+                    aria-labelledby="menu-data-laporan">
                     <li>
-                        <a href="{{ route('laporan-bibit.index') }}"
-                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Laporan Bibit Berkualitas</a>
+                        <a href="#">
+                            <span class="icon-[mdi--seed-outline] size-5"></span>
+                            Laporan Bibit
+                        </a>
                     </li>
                     <li>
-                        <a href="#"
-                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Laporan Hibah Alat</a>
+                        <a href="#">
+                            <span class="icon-[mdi--donation-outline] size-5"></span>
+                            Laporan Hibah
+                        </a>
                     </li>
                 </ul>
             </li>
-
-            {{-- Menu Manajemen Admin --}}
             <li>
-                <a href="#"
-                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <svg class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                        height="24" fill="currentColor" viewBox="0 0 24 24">
-                        <path fill-rule="evenodd"
-                            d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z"
-                            clip-rule="evenodd" />
-                    </svg>
-                    <span class="flex-1 ms-3 whitespace-nowrap">Kelola Data Admin</span>
+                <a href="#">
+                    <span class="icon-[iconoir--user-crown] size-5"></span>
+                    Data Admin
                 </a>
             </li>
         </ul>
