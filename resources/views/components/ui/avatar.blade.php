6a16bc4cbc26678f187239740e36e38b6ee1215f
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-16 15:13:26 +0700

    menghapus komponen yang tidak dibutuhkan

--- resources/views/components/ui/avatar.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="w-fit h-fit">
-    <img class="w-10 h-10 rounded-full shadow-sm" src="#" alt="Rounded avatar">
-</div>
--- resources/views/components/ui/icon.blade.php
+++
@@ -1,5 +0,0 @@
-<span class="inline-block {{ $position ?? 'static'}}" style="width: {{ $size ?? 24}}px; height: {{ $size ?? 24 }}px; color: {{ $color ?? 'currentColor'}};">
-    <span class="w-full h-full block">
-        {{ $slot }}
-    </span>
-</span>
