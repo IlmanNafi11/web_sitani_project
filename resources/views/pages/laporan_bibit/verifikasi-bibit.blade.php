367f403c9051a27eafc132137569443a042b56b0
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-16 15:08:13 +0700

    memperbarui layout

--- resources/views/pages/laporan_bibit/verifikasi-bibit.blade.php
+++ resources/views/pages/laporan_bibit/verifikasi-bibit.blade.php
@@ -1,24 +1,10 @@
 @extends('layouts.layout')
 @section('content')
-    <div class="kelompok-tani-page-content flex flex-col w-full h-screen relative overflow-hidden">
-        <x-partials.header />
-        <div class="content-container flex w-full h-[calc(100vh-70px)] absolute top-[70px]">
-            <x-partials.side-bar />
-            <div class="content flex-1 w-full p-8 flex">
-                <div
-                    class="main-content w-full overflow-y-auto p-6 rounded-xl bg-white border border-[#E5E8EC] h-fit max-h-full">
-                    <div class="flex flex-col gap-4">
-                        <div class="border-b border-[#E5E8EC]">
-                            <span class="title-label-page font-medium mb-4 block">Verifikasi laporan</span>
-                        </div>
-                        <div class="form-container">
-                            <x-form.form-bibit-verify :laporan="$laporan"/>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <x-ui.card>
+        <div class="mb-5 border-b border-[#E5E8EC]">
+            <x-ui.title :title="'Verifikasi Laporan'" />
         </div>
-    </div>
-    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
-        crossorigin="anonymous"></script>
+        <x-form.form-bibit-verify :laporan="$laporan" />
+    </x-ui.card>
 @endsection
+
