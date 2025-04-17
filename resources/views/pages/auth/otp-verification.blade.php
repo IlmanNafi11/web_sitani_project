c681f64ddd3ea0936a54363dce72e275b0f824fb
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-17 21:05:51 +0700

    membuat halaman verifikasi otp

---
+++ resources/views/pages/auth/otp-verification.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.auth-layout')
+
+@section('title', 'Verifikasi OTP | Sitani')
+@section('content')
+    <x-ui.card>
+        <form id="verif-otp" action="{{ route('reset-password') }}" method="GET">
+            <div class="card-header">
+                <span class="block w-full text-center text-3xl" style="font-family: Marck Script">Verifikasi OTP</span>
+            </div>
+            <div class="card-body">
+                <x-form.pin-input />
+                <button type="submit" id="verif-button" class="btn w-full btn-soft btn-accent mt-6">
+                    <span class="icon-[solar--verified-check-broken] size-4.5 shrink-0"></span>Verifikasi
+                </button>
+            </div>
+            <div class="card-footer">
+                <span class="label-text block text-center"><a href="{{ route('verifikasi-email') }}"
+                        class="link link-accent link-animated">Kembali</a></span>
+            </div>
+        </form>
+    </x-ui.card>
+@endsection
