015bf830517b9cd2ac65f218c4450c0cf27adb64
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-17 20:49:58 +0700

    membuat komponen pin input

---
+++ resources/views/components/form/pin-input.blade.php
@@ -0,0 +1,14 @@
+<div class="flex space-x-3 w-full" data-pin-input='{"availableCharsRE": "^[0-9]+$"}'>
+    <input type="text" class="pin-input pin-input-underline focus:outline-none outline-none focus:ring-0" name="otp[]"
+        placeholder="○" data-pin-input-item />
+    <input type="text" class="pin-input pin-input-underline focus:outline-none outline-none focus:ring-0" name="otp[]"
+        placeholder="○" data-pin-input-item />
+    <input type="text" class="pin-input pin-input-underline focus:outline-none outline-none focus:ring-0" name="otp[]"
+        placeholder="○" data-pin-input-item />
+    <input type="text" class="pin-input pin-input-underline focus:outline-none outline-none focus:ring-0" name="otp[]"
+        placeholder="○" data-pin-input-item />
+    <input type="text" class="pin-input pin-input-underline focus:outline-none outline-none focus:ring-0" name="otp[]"
+        placeholder="○" data-pin-input-item />
+    <input type="text" class="pin-input pin-input-underline focus:outline-none outline-none focus:ring-0" name="otp[]"
+        placeholder="○" data-pin-input-item />
+</div>
