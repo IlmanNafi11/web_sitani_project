e541c5081e55fbfb3cf5061f0f8e602bb17ed58b
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-18 16:08:45 +0700

    membuat admin service

---
+++ app/Services/AdminService.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Interfaces\CrudInterface;
+use Illuminate\Support\Facades\Log;
+
+class AdminService
+{
+    protected CrudInterface $repository;
+    public function __construct(CrudInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function getAll()
+    {
+        try {
+            return $this->repository->getAll();
+        } catch (\Throwable $th) {
+            Log::error('Gagal mengambil seluruh data admin: ' . $th->getMessage());
+        }
+    }
+
+    public function getById($id)
+    {
+        try {
+            return $this->repository->find($id);
+        } catch (\Throwable $th) {
+            Log::error('Gagal mengambil data admin berdasarkan id: ' . $th->getMessage());
+        }
+    }
+
+    public function create(array $data)
+    {
+        try {
+            $dt = [
+                'email' => $data['email'],
+                'nama' => $data['nama'],
+                'no_hp' => $data['no_hp'],
+                'alamat' => $data['alamat'],
+                'password' => 'sitani',
+                'role' => 'admin',
+                'is_password_set' => false,
+            ];
+
+            return $this->repository->create($dt);
+        } catch (\Throwable $th) {
+            Log::error('Gagal menyimpan data admin: ' . $th->getMessage());
+        }
+    }
+
+    public function update($id, array $data)
+    {
+        try {
+            return $this->repository->update($id, $data);
+        } catch (\Throwable $th) {
+            Log::error('Gagal memperbarui data admin: ' . $th->getMessage());
+        }
+    }
+
+    public function delete($id)
+    {
+        try {
+            return $this->repository->delete($id);
+        } catch (\Throwable $th) {
+            Log::error('Gagal menghapus data admin: ' . $th->getMessage());
+        }
+    }
+}
