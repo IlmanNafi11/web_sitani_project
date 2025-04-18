6774f96d06f24e0fb2aa1bc33e54dc44980ab597
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-18 16:10:36 +0700

    implementasi controller untuk handle request create data dan hapus data admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,16 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AdminRequest;
+use App\Services\AdminService;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    protected AdminService $service;
+
+    public function __construct(AdminService $service)
+    {
+        $this->service = $service;
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $admins = $this->service->getAll();
+        return view('pages.admin.index', compact('admins'));
     }
 
     /**
@@ -19,15 +29,20 @@ class AdminController extends Controller
      */
     public function create()
     {
-        //
+        return view('pages.admin.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(AdminRequest $request)
     {
-        //
+        $result = $this->service->create($request->validated());
+
+        if ($result) {
+            return redirect()->route('admin.index')->with('success', 'Data berhasil disimpan');
+        }
+        return redirect()->route('admin.index')->with('failed', 'Data gagal disimpan');
     }
 
     /**
@@ -59,6 +74,8 @@ class AdminController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $this->service->delete($id);
+
+        return redirect()->route('admin.index')->with('success', 'Data berhasil dihapus');
     }
 }
