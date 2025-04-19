93aa47938ba8ca9c5703cc7a56bfe75defe5f9a0
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-19 13:31:27 +0700

    refactoring dan menambahkan validasi

--- resources/views/components/form/pin-input.blade.php
+++ resources/views/components/form/pin-input.blade.php
@@ -1,14 +1,21 @@
 <div class="flex space-x-3 w-full" data-pin-input='{"availableCharsRE": "^[0-9]+$"}'>
-    <input type="text" class="pin-input pin-input-underline focus:outline-none outline-none focus:ring-0" name="otp[]"
-        placeholder="○" data-pin-input-item />
-    <input type="text" class="pin-input pin-input-underline focus:outline-none outline-none focus:ring-0" name="otp[]"
-        placeholder="○" data-pin-input-item />
-    <input type="text" class="pin-input pin-input-underline focus:outline-none outline-none focus:ring-0" name="otp[]"
-        placeholder="○" data-pin-input-item />
-    <input type="text" class="pin-input pin-input-underline focus:outline-none outline-none focus:ring-0" name="otp[]"
-        placeholder="○" data-pin-input-item />
-    <input type="text" class="pin-input pin-input-underline focus:outline-none outline-none focus:ring-0" name="otp[]"
-        placeholder="○" data-pin-input-item />
-    <input type="text" class="pin-input pin-input-underline focus:outline-none outline-none focus:ring-0" name="otp[]"
-        placeholder="○" data-pin-input-item />
+    @for ($i = 0; $i < 6; $i++)
+        <input type="text"
+               class="pin-input pin-input-underline focus:outline-none outline-none focus:ring-0 {{ $errors->has('otp') || $errors->has('otp.' . $i) ? 'border border-b-red-500! placeholder:text-red-500! text-red-500!' : '' }}"
+               name="otp[]"
+               placeholder="○"
+               data-pin-input-item
+               value="{{ old('otp.' . $i) }}" />
+    @endfor
 </div>
+
+@if ($errors->has('otp'))
+    <p class="text-red-500 text-sm mt-1">{{ $errors->first('otp') }}</p>
+@else
+    @for ($i = 0; $i < 6; $i++)
+        @if ($errors->has('otp.' . $i))
+            <p class="text-red-500 text-sm mt-1">{{ $errors->first('otp.' . $i) }}</p>
+            @break
+        @endif
+    @endfor
+@endif
