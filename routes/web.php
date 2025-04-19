79b3d37c8f910c4e2983f5a7dfa230465abd41db
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-19 17:42:22 +0700

    memproteksi route berbasis session

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Auth\AuthWebController;
 use App\Http\Controllers\BibitBerkualitasController;
 use App\Http\Controllers\DesaController;
 use App\Http\Controllers\KecamatanController;
@@ -10,34 +11,65 @@ use App\Http\Controllers\LaporanBibitController;
 use App\Http\Controllers\PenyuluhTerdaftarController;
 use Illuminate\Support\Facades\Route;
 
-// sementara
-Route::get('/', function () {
-    return view('pages.auth.login');
-})->name('login');
-Route::get('verif-email', fn() => view('pages.auth.email-verification'))->name('verifikasi-email');
-Route::get('verifikasi-otp', fn() => view('pages.auth.otp-verification'))->name('verifikasi-otp');
-Route::get('perbaru-sandi', fn() => view('pages.auth.reset-password'))->name('reset-password');
-Route::get('/dashboard', function() {
-    return view('pages.dashboard');
-})->name('dashboard.admin');
+/**
+ * Auth Routes(Tanpa middleware)
+ */
+Route::controller(AuthWebController::class)->group(function() {
+    Route::get('admin', static fn() => view('pages.auth.login'))->name('login');
+    Route::post('admin', 'login')->name('login.post');
 
-Route::resource('bibit', BibitBerkualitasController::class);
-Route::resource('desa', DesaController::class);
-Route::resource('kecamatan', KecamatanController::class);
-Route::resource('kelompok-tani', KelompokTaniController::class);
-Route::resource('komoditas', KomoditasController::class);
-Route::resource('penyuluh-terdaftar', PenyuluhTerdaftarController::class);
-Route::resource('laporan-bibit', LaporanBibitController::class);
+    Route::get('admin/setup-password', static fn() => view('pages.auth.setup-password'))->name('setup-password');
+    Route::post('admin/setup-password', 'setupPassword')->name('setup-password.post');
 
-Route::controller(AdminController::class)->group(callback: function() {
-    Route::get('admin/admin-users', 'index')->name('admin.index');
-    Route::get('admin/admin-users/create', 'create')->name('admin.create');
-    Route::post('admin/admin-users', 'store')->name('admin.store');
-    Route::delete('admin/admin-users/{id}', 'destroy')->name('admin.destroy');
-    Route::get('admin/profil', 'profile')->name('admin.profile');
-    Route::get('admin/logout', 'logout')->name('admin.logout');
+    Route::get('verif-email', fn() => view('pages.auth.email-verification'))->name('verifikasi-email');
+    Route::post('verif-email','sendForgotPasswordEmail')->name('verifikasi-email.post');
 });
 
-Route::get('test-component', fn()=> view('test-components'));
+/**
+ * Auth Routes(Terproteksi)
+ * - active.session
+ * @see \App\Http\Middleware\ActiveSessionMiddleware
+ *
+ */
+Route::middleware('otp.session')->group(function () {
+    Route::controller(AuthWebController::class)->group(function() {
+        Route::get('verifikasi-otp', 'showOtpVerificationForm')->name('verifikasi-otp');
+        Route::post('verifikasi-otp', 'verifyOtp')->name('verifikasi-otp.post');
+        Route::post('verifikasi-otp/resend', 'resendOtp')->name('verifikasi-otp.resend');
+        Route::get('perbaru-sandi', 'showResetPasswordForm')->name('reset-password');
+        Route::post('perbaru-sandi', 'performPasswordReset')->name('reset-password.post');
+    });
+});
+
+/**
+ * Route Panel Admin(Terproteksi)
+ * - active.session
+ * @see \App\Http\Middleware\ActiveSessionMiddleware
+ *
+ */
+Route::middleware('active.session')->group(function() {
+    Route::get('admin/dashboard', fn() => view('pages.dashboard'))->name('dashboard.admin');
+
+    Route::resource('admin/bibit', BibitBerkualitasController::class);
+    Route::resource('admin/desa', DesaController::class);
+    Route::resource('admin/kecamatan', KecamatanController::class);
+    Route::resource('admin/kelompok-tani', KelompokTaniController::class);
+    Route::resource('admin/komoditas', KomoditasController::class);
+    Route::resource('admin/penyuluh-terdaftar', PenyuluhTerdaftarController::class);
+    Route::resource('admin/laporan-bibit', LaporanBibitController::class);
+
+    Route::controller(AdminController::class)->group(function() {
+        Route::get('admin/admin-users', 'index')->name('admin.index');
+        Route::get('admin/admin-users/create', 'create')->name('admin.create');
+        Route::post('admin/admin-users', 'store')->name('admin.store');
+        Route::delete('admin/admin-users/{id}', 'destroy')->name('admin.destroy');
+        Route::get('admin/profil', 'profile')->name('admin.profile');
+    });
+
+    Route::post('admin/logout',[AuthWebController::class, 'logout'])->name('admin.logout');
+});
+
+// Untuk sementara nggk aku proteksi
+Route::get('test-component', fn() => view('test-components'));
 Route::get('kecamatan/{id}/desa', [DesaController::class, 'getByKecamatanId']);
 Route::get('kecamatan/{id}/penyuluh', [PenyuluhTerdaftarController::class, 'getByKecamatanId']);
