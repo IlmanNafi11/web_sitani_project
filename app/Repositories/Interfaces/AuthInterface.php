7c5de4bd73cf21e984188f34c60565f4e64a0113
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-19 10:32:54 +0700

    menambahkan kontrak find user generate and save otp, validate otp, dan invalidate otp

--- app/Repositories/Interfaces/AuthInterface.php
+++ app/Repositories/Interfaces/AuthInterface.php
@@ -6,5 +6,9 @@ use App\Models\User;
 
 interface AuthInterface
 {
+    public function findUser(array $conditions, bool $multiple = false): ?User;
     public function resetPassword(User $user, string $password): bool;
+    public function generateAndSaveOtp(User $user): string;
+    public function validateOtp(User $user, string $code): bool;
+    public function invalidateOtps(User $user): void;
 }
