fed2e58612d22ed3d8545df7922302d504702551
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-21 11:26:37 +0700

    mengatur starter seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,18 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-//        User::create([
-//            'email' => 'admin@gmail.com',
-//            'email_verified_at' => now(),
-//            'password' => bcrypt('password'),
-//            'role' => 'super admin',
-//            'created_at' => now(),
-//            'updated_at' => now(),
-//        ]);
-
+        $this->call(RoleSeeder::class);
         $this->call(PermissionSeeder::class);
-
+        $this->call(UserSeeder::class);
     }
 }
