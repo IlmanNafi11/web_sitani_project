59a02783c869ef3a63855bc67b0e609a3fbee691
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-21 11:26:00 +0700

    membuat seeder RoleSeeder dan UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'email' => 'fajarfadhilah510@gmail.com',
+            'password' => bcrypt("sitani"),
+            'email_verified_at' => now(),
+            'is_password_set' => false,
+        ]);
+
+        $user->assignRole('super admin');
+
+        Admin::create([
+            'user_id' => $user->id,
+            'nama' => 'Ilman Nafi',
+            'no_hp' => "085555123456",
+            'alamat' => "Jl. Merapi No. 123",
+        ]);
+    }
+}
