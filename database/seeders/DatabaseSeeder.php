a999e18cb04ed8bdda1d29b660b57e3cc67b91cb
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-24 09:14:19 +0700

    menambahkan seeder penyuluh

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(PermissionSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(RolePermissionSeeder::class);
+        $this->call(PenyuluhSeeder::class);
     }
 }
