3b98da006833d8280bc71b307bb773d43938ca09
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-26 11:25:30 +0700

    implementasi fungsional reset password

--- app/Http/Controllers/Auth/AuthApiController.php
+++ app/Http/Controllers/Auth/AuthApiController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\EmailRequest;
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\OtpCodeRequest;
+use App\Http\Requests\PasswordRequest;
 use App\Http\Requests\PenyuluhRequest;
 use App\Http\Requests\PhonePenyuluhRequest;
 use App\Http\Resources\UserLoginResource;
 use App\Services\PenyuluhService;
 use App\Services\PenyuluhTerdaftarService;
+use App\Services\UserService;
 use App\Trait\ApiResponse;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Facades\JWTFactory;
@@ -21,11 +24,13 @@ class AuthApiController extends Controller
     use ApiResponse;
     protected PenyuluhService $service;
     protected PenyuluhTerdaftarService $penyuluhTerdaftarService;
+    protected UserService $userService;
 
-    public function __construct(PenyuluhService $service, PenyuluhTerdaftarService $penyuluhTerdaftarService)
+    public function __construct(PenyuluhService $service, PenyuluhTerdaftarService $penyuluhTerdaftarService, UserService $userService)
     {
         $this->service = $service;
         $this->penyuluhTerdaftarService = $penyuluhTerdaftarService;
+        $this->userService = $userService;
     }
 
     /**
@@ -34,7 +39,7 @@ class AuthApiController extends Controller
      * @param LoginRequest $request Form request
      * @return JsonResponse response
      */
-    public function login(LoginRequest $request)
+    public function login(LoginRequest $request): JsonResponse
     {
         $credentials = $request->validated();
 
@@ -59,7 +64,7 @@ class AuthApiController extends Controller
      * @param PhonePenyuluhRequest $request Form request
      * @return JsonResponse response
      */
-    public function validatePhone(PhonePenyuluhRequest $request)
+    public function validatePhone(PhonePenyuluhRequest $request): JsonResponse
     {
         $result = $this->penyuluhTerdaftarService->getByPhone($request->validated('no_hp'));
         if ($result['success']) {
@@ -75,7 +80,7 @@ class AuthApiController extends Controller
      * @param PenyuluhRequest $request Form request
      * @return JsonResponse response
      */
-    public function register(PenyuluhRequest $request)
+    public function register(PenyuluhRequest $request): JsonResponse
     {
         $result = $this->service->create($request->validated());
 
@@ -108,5 +113,93 @@ class AuthApiController extends Controller
             'expires_in' => JWTFactory::getTTL() * 60,
             'user' => new UserLoginResource($user),
         ], $message);
+    }
+
+    /**
+     * Mengirim kode OTP untuk reset password
+     *
+     * @param EmailRequest $request Form request
+     * @return JsonResponse response
+     */
+    public function sendOtp(EmailRequest $request): JsonResponse
+    {
+        $validated = $request->validated();
+
+        $data = $this->userService->findUser(['email' => $validated['email']]);
+        $user = null;
+        if ($data['success']) {
+            $user = $data['data'];
+        }
+
+        if (!$user || !$user->hasRole('penyuluh')) {
+            return $this->errorResponse("Anda tidak memiliki akses, Gunakan akun penyuluh untuk masuk", 403);
+        }
+
+        $result = $this->userService->sendOtpToEmail($user);
+
+        if (!$result['success']) {
+            return $this->errorResponse('Gagal mengirim OTP', 401);
+        }
+
+        return $this->successResponse([
+            'id' => $user->id,
+            'email' => $validated['email'],
+        ], $result['message']);
+    }
+
+    /**
+     * Validasi kode OTP
+     *
+     * @param OtpCodeRequest $request Form request
+     * @return JsonResponse
+     */
+    public function validateOtp(OtpCodeRequest $request): JsonResponse
+    {
+        $validated = $request->validated();
+        $email = $request->input('email');
+        $data = $this->userService->findUser(['email' => $email]);
+
+        if (!$data['success']) {
+            return $this->errorResponse('Gagal mengirim OTP', 401);
+        }
+
+        $user = $data['data'];
+        $result = $this->userService->verifyOtp($user, $validated);
+
+        if (!$result['success']) {
+            return $this->errorResponse($result['message'], 401);
+        }
+
+        return $this->successResponse(['email' => $email], $result['message']);
+    }
+
+    /**
+     * Memperbarui kata sandi
+     *
+     * @param PasswordRequest $request Form request
+     * @return JsonResponse
+     */
+    public function passwordReset(PasswordRequest $request): JsonResponse
+    {
+        $validated = $request->validated();
+        $email = $request->input('email');
+        $data = $this->userService->findUser(['email' => $email]);
+        $user = null;
+
+        if ($data['success']) {
+            $user = $data['data'];
+        }
+
+        if (!$data['success']) {
+            return $this->errorResponse($data['message'], 401);
+        }
+
+        $this->userService->invalidateOtps($user);
+        $result = $this->userService->resetPassword($user, $validated['password']);
+        if (!$result['success']) {
+            return $this->errorResponse($result['message'], 401);
+        }
+
+        return $this->successResponse(['email' => $email], $result['message']);
     }
 }
