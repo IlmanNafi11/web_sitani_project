b93843fa8eb92d65588bad7e91f64837125b70d9
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-26 07:17:00 +0700

    menambahkan param  withRelations untuk counter get data beserta relasi

--- app/Repositories/Interfaces/AuthInterface.php
+++ app/Repositories/Interfaces/AuthInterface.php
@@ -6,7 +6,7 @@ use App\Models\User;
 
 interface AuthInterface
 {
-    public function findUser(array $conditions, bool $multiple = false): ?User;
+    public function findUser(array $conditions, bool $multiple = false, array $withRelations = []): ?User;
     public function resetPassword(User $user, string $password): bool;
     public function generateAndSaveOtp(User $user): string;
     public function validateOtp(User $user, string $code): bool;
