5bd51968a790cfb8f9bf9a283500ded33c4457f2
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-26 12:57:35 +0700

    implementasi get by penyuluh id diservice layer

--- app/Services/LaporanBibitService.php
+++ app/Services/LaporanBibitService.php
@@ -4,16 +4,19 @@ namespace App\Services;
 
 use App\Models\LaporanKondisi;
 use App\Repositories\Interfaces\CrudInterface;
+use App\Repositories\Interfaces\LaporanCustomQueryInterface;
 use Illuminate\Support\Facades\Log;
 
 class LaporanBibitService
 {
 
     protected CrudInterface $repository;
+    protected LaporanCustomQueryInterface $query;
 
-    public function __construct(CrudInterface $repository)
+    public function __construct(CrudInterface $repository, LaporanCustomQueryInterface $query)
     {
         $this->repository = $repository;
+        $this->query = $query;
     }
 
     /**
@@ -209,6 +212,39 @@ class LaporanBibitService
             return [
                 'success' => false,
                 'message' => 'Gagal menghapus data laporan bibit',
+                'data' => [],
+            ];
+        }
+    }
+
+    public function getByPenyuluhId(string|int $id): array
+    {
+        try {
+            $conditions = ['penyuluh_id' => $id];
+            $relations = ['penyuluh', 'penyuluh.penyuluhTerdaftar', 'komoditas', 'laporanKondisiDetail',];
+            $result = $this->query->getByPenyuluhId($conditions, $relations);
+            if ($result->isNotEmpty()) {
+                return [
+                    'success' => true,
+                    'message' => 'Laporan bibit ditemukan',
+                    'data' => $result,
+                ];
+            }
+            return [
+                'success' => false,
+                'message' => 'Laporan bibit tidak ditemukan',
+                'data' => [],
+            ];
+        } catch (\Throwable $th) {
+            Log::error('Terjadi kesalahan saat mengambil data laporan bibit.', [
+                'source' => __METHOD__,
+                'error' => $th->getMessage(),
+                'trace' => $th->getTraceAsString(),
+            ]);
+
+            return [
+                'success' => false,
+                'message' => 'Gagal mengambil data laporan bibit',
                 'data' => [],
             ];
         }
