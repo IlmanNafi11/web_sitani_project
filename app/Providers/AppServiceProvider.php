5ad856d828c6978ca92a697c835ec530c0e0bc99
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-26 12:55:14 +0700

    binding laporan bibit service dengan repository untuk kontrak custom query

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,7 @@ use App\Repositories\Interfaces\AuthInterface;
 use App\Repositories\Interfaces\CrudInterface;
 use App\Repositories\Interfaces\DesaCustomQueryInterface;
 use App\Repositories\Interfaces\KelompokTaniCustomQueryInterface;
+use App\Repositories\Interfaces\LaporanCustomQueryInterface;
 use App\Repositories\Interfaces\ManyRelationshipManagement;
 use App\Repositories\Interfaces\PenyuluhTerdaftarCustomQueryInterface;
 use App\Repositories\Interfaces\RoleRepositoryInterface;
@@ -60,6 +61,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->when(RoleService::class)->needs(RoleRepositoryInterface::class)->give(RoleRepository::class);
         $this->app->when(PenyuluhService::class)->needs(CrudInterface::class)->give(PenyuluhRepository::class);
         $this->app->when(KelompokTaniService::class)->needs(KelompokTaniCustomQueryInterface::class)->give(KelompokTaniRepository::class);
+        $this->app->when(LaporanBibitService::class)->needs(LaporanCustomQueryInterface::class)->give(LaporanBibitRepository::class);
     }
 
     /**
