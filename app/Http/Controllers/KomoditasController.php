972d6d77a6adb7f7b87400571bd48a1569647fe3
IlmanNafi11 <studyilmannafi@gmail.com>
2025-04-27 17:24:32 +0700

    implementasi di controller untuk handle request import export data excel

--- app/Http/Controllers/KomoditasController.php
+++ app/Http/Controllers/KomoditasController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Exports\KomoditasExport;
+use App\Exports\template\KomoditasTemplate;
 use App\Http\Requests\KomoditasRequest;
+use App\Imports\KomoditasImport;
 use App\Services\KomoditasService;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\View\View;
+use Maatwebsite\Excel\Facades\Excel;
 
 class KomoditasController extends Controller
 {
@@ -91,5 +97,41 @@ class KomoditasController extends Controller
             return redirect()->route('komoditas.index')->with('success', 'Data berhasil dihapus');
         }
         return redirect()->route('komoditas.index')->with('error', 'Data Gagal dihapus');
+    }
+
+    public function downloadTemplate()
+    {
+        return Excel::download(new KomoditasTemplate(), 'komoditas.xlsx');
+    }
+
+    public function import(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'file' => 'required|mimes:xlsx,xls'
+        ]);
+
+        try {
+            $import = new KomoditasImport();
+            Excel::import($import, $request->file('file'));
+
+            $failures = $import->getFailures();
+
+            if (!empty($failures)) {
+                return redirect()->route('komoditas.index')->with([
+                    'success' => 'Data berhasil diimport, namun ada beberapa data yang gagal.',
+                    'failures' => $failures
+                ]);
+            }
+
+            return redirect()->route('komoditas.index')->with('success', 'Data berhasil diimport');
+        } catch (\Exception $e) {
+            Log::info('error template');
+            return redirect()->route('komoditas.index')->with('error', $e->getMessage());
+        }
+    }
+
+    public function export()
+    {
+        return Excel::download(new KomoditasExport(), 'komoditas.xlsx');
     }
 }
