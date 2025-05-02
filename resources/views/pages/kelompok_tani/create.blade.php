1d8a4c2bb1e8555e5987f52b3ee0339473781a85
IlmanNafi11 <studyilmannafi@gmail.com>
2025-05-02 21:25:29 +0700

    fix bug placeholder tidak muncul ketika isMultiple true dan masalah option tidak diatur kembali setelah select kecamatan on change

--- resources/views/pages/kelompok_tani/create.blade.php
+++ resources/views/pages/kelompok_tani/create.blade.php
@@ -3,23 +3,32 @@
 @section('content')
     <x-ui.card>
         <div class="mb-5 border-b border-[#E5E8EC]">
-            <x-ui.title :title="'Tambah Data Kelompok Tani'" />
+            <x-ui.title :title="'Tambah Data Kelompok Tani'"/>
         </div>
         <form id="form-tambah-kelompok-tani" action="{{ route('kelompok-tani.store') }}" method="post">
             @csrf
             <div class="w-full grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
-                <x-form.input-text :keyId="'nama'" :label="'Nama Kelompok Tani'" :name="'nama'" :placeholder="'Masukan nama kelompok tani'" />
+                <x-form.input-text :keyId="'nama'" :label="'Nama Kelompok Tani'" :name="'nama'"
+                                   :placeholder="'Masukan nama kelompok tani'"/>
                 <x-form.advanced-select :name="'kecamatan_id'" :options="$kecamatans" :keyId="'kecamatan_id'"
-                    :label="'Kecamatan'" :placeholder="'Pilih kecamatan'" :optionValue="'id'" :selected="null"
-                    :optionLabel="'nama'" :helperText="'Pilih kecamatan terlebih dahulu untuk menampilkan opsi desa dan penyuluh'" />
+                                        :hasSearch="true"
+                                        :label="'Kecamatan'" :placeholder="'Pilih kecamatan'" :optionValue="'id'"
+                                        :selected="null"
+                                        :optionLabel="'nama'"
+                                        :helperText="'Pilih kecamatan terlebih dahulu untuk menampilkan opsi desa dan penyuluh'"/>
                 <x-form.advanced-select :name="'desa_id'" :options="null" :keyId="'desa_id'" :label="'Desa'"
-                    :placeholder="'Pilih desa'" :helperText="'Pilih kecamatan terlebih dahulu untuk menampilkan opsi'" />
-                <x-form.advanced-select :name="'penyuluh_terdaftar_id[]'" :options="null" :keyId="'penyuluh_terdaftar_id'"
-                    :label="'Penyuluh'" :placeholder="'Pilih penyuluh'" :helperText="'Pilih kecamatan terlebih dahulu untuk menampilkan opsi'" :isMultiple="true" :hasSearch="true" :searchPlaceholder="'Cari Penyuluh'" />
+                                        :placeholder="'Pilih desa'"
+                                        :helperText="'Pilih kecamatan terlebih dahulu untuk menampilkan opsi'"
+                                        :hasSearch="true"/>
+                <x-form.advanced-select :name="'penyuluh_terdaftar_id[]'" :options="null"
+                                        :keyId="'penyuluh_terdaftar_id'"
+                                        :label="'Penyuluh'" :placeholder="'Pilih penyuluh'"
+                                        :helperText="'Pilih kecamatan terlebih dahulu untuk menampilkan opsi'"
+                                        :isMultiple="true" :hasSearch="true" :searchPlaceholder="'Cari Penyuluh'"/>
             </div>
             <div class="flex gap-2.5">
-                <x-ui.button.back-button :style="'btn-soft'" :title="'Kembali'" :routes="'kelompok-tani.index'" />
-                <x-ui.button.save-button formId="form-tambah-kelompok-tani" :style="'btn-soft'" />
+                <x-ui.button.back-button :style="'btn-soft'" :title="'Kembali'" :routes="'kelompok-tani.index'"/>
+                <x-ui.button.save-button formId="form-tambah-kelompok-tani" :style="'btn-soft'"/>
             </div>
         </form>
     </x-ui.card>
@@ -28,12 +37,7 @@
             async function getDesa(kecamatan_id) {
                 try {
                     const response = await axios.get(`/kecamatan/${kecamatan_id}/desa`);
-                    if (Array.isArray(response.data)) {
-                        return response.data;
-                    }
-
-                    alert(response.data.message || 'Data desa tidak tersedia');
-                    return [];
+                    return Array.isArray(response.data) ? response.data : [];
                 } catch (error) {
                     console.error('Gagal mengambil data desa:', error);
                     return [];
@@ -43,12 +47,7 @@
             async function getPenyuluh(kecamatan_id) {
                 try {
                     const response = await axios.get(`/kecamatan/${kecamatan_id}/penyuluh`);
-                    if (Array.isArray(response.data)) {
-                        return response.data;
-                    }
-
-                    alert(response.data.message || 'Data penyuluh tidak tersedia');
-                    return [];
+                    return Array.isArray(response.data) ? response.data : [];
                 } catch (error) {
                     console.error('Gagal mengambil data penyuluh', error);
                     return [];
@@ -71,17 +70,10 @@
                     const desaList = await getDesa(kecamatan_id);
                     const penyuluhList = await getPenyuluh(kecamatan_id);
 
-                    const currentOptions = Array.from(document.querySelector('#desa_id').options)
-                        .map(opt => opt.value)
-                        .filter(val => val !== '');
-
-                    currentOptions.forEach(val => desaSelect.removeOption(val));
-
-                    const currentOptionsPenyuluh = Array.from(document.querySelector('#penyuluh_terdaftar_id').options)
-                        .map(opt => opt.val)
-                        .filter(val => val !== '');
-
-                    currentOptionsPenyuluh.forEach(val => penyuluhSelect.removeOption(val));
+                    const desaOptionValues = Array.from(document.querySelector('#desa_id').options).map(opt => opt.value);
+                    desaOptionValues.forEach(val => {
+                        if (val !== '') desaSelect.removeOption(val);
+                    });
 
                     if (desaList.length > 0) {
                         const options = desaList.map(desa => ({
@@ -90,6 +82,11 @@
                         }));
                         desaSelect.addOption(options);
                     }
+
+                    const penyuluhOptionValues = Array.from(document.querySelector('#penyuluh_terdaftar_id').options).map(opt => opt.value);
+                    penyuluhOptionValues.forEach(val => {
+                        if (val !== '') penyuluhSelect.removeOption(val);
+                    });
 
                     if (penyuluhList.length > 0) {
                         const options = penyuluhList.map(penyuluh => ({
@@ -101,5 +98,6 @@
                 });
             });
         });
+
     </script>
 @endsection
